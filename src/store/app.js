8b308619d4ac4e8e21f3d04c103b65cff2b0dff3
ukdong <dontBlamestorming@gmail.com>
2022-06-29 16:57:47 +0900

    FEAT: Add Dialog state && setState in RTK

--- src/store/app.js
+++ src/store/app.js
@@ -1,20 +1,29 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { getUsersInfo } from "api/auth";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getUsersInfo } from 'api/auth';
 
 const initialState = {
-  currentMenu: "/",
+  currentMenu: '/',
+
+  isOpenDialog: {
+    resultOfIssueUserId: false,
+    resultOfIssueMiddleAdminId: false,
+  },
+
   storeList: [],
 };
 
 export const appSlice = createSlice({
-  name: "app",
+  name: 'app',
   initialState,
   reducers: {
     setCurrentMenu: (state, action) => {
       state.currentMenu = action.payload;
     },
+    setIsOpenDialog: ({ isOpenDialog }, action) => {
+      isOpenDialog[action.payload.name] = action.payload.status;
+    },
   },
 });
 
-export const { setCurrentMenu } = appSlice.actions;
+export const { setCurrentMenu, setIsOpenDialog } = appSlice.actions;
 export default appSlice.reducer;
