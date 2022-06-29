b252e7515ee076b5b56c4fc359a0b458b9e9687d
ukdong <dontBlamestorming@gmail.com>
2022-06-30 00:33:20 +0900

    STYLE: Move constants in directory

--- src/constant/index.jsx
+++ src/constant/index.jsx
@@ -20,3 +20,42 @@ export const HOME_ROUTE_BUTTONS = [
     path: 'middle-admin',
   },
 ];
+
+export const MIDDLE_ADMIN_INPUTFIELD = [
+  {
+    title: '중간관리자 ID',
+    placeholder: 'ID를 입력해주세요.',
+    button: {
+      has: true,
+      type: 'checkDuplicate',
+      name: '중복검사',
+    },
+  },
+  {
+    title: '관리자명',
+    placeholder: '관리자명을 입력해주세요.',
+    button: {
+      has: true,
+      type: 'checkDuplicate',
+      name: '중복검사',
+    },
+  },
+  {
+    title: '이메일',
+    placeholder: '대표 이메일을 입력해주세요.',
+    button: {
+      has: true,
+      type: 'checkDuplicate',
+      name: '중복검사',
+    },
+  },
+  {
+    title: '비밀번호',
+    placeholder: '비밀번호를 입력해주세요.',
+    button: {
+      has: true,
+      type: 'autoGenerate',
+      name: '자동생성',
+    },
+  },
+];
